9a746c18262814916f9b0bb383aa4872c1728d13
123456687548 <essertim13@googlemail.com>
2019-04-26 15:20:29 +0200

    Kategorie hinzugefügt

--- src/main/java/com/example/demo/factories/RezeptFactory.java
+++ src/main/java/com/example/demo/factories/RezeptFactory.java
@@ -3,6 +3,7 @@ package com.example.demo.factories;
 import com.example.demo.entities.Mitarbeiter;
 import com.example.demo.entities.Rezept;
 import com.example.demo.entities.Speisekarte;
+import com.example.demo.valueObjects.Kategorie;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,9 +11,10 @@ public class RezeptFactory {
 
     public RezeptFactory (){}
 
-    public Rezept createRezept (String speisekartenNamen, String rezeptNamen, String rezeptBeschreibung, String rezeptZutaten, String mitarbeiterNamen, String mitarbeiterVornamen){
+    public Rezept createRezept (String speisekartenNamen, String rezeptNamen, String rezeptBeschreibung, String rezeptZutaten, String mitarbeiterNamen, String mitarbeiterVornamen, String kategorie){
         Speisekarte speisekarte1 = new Speisekarte(speisekartenNamen);
-        Rezept r1 = new Rezept(rezeptNamen, rezeptBeschreibung, rezeptZutaten, speisekarte1);
+        Kategorie kategorieObj = new Kategorie(kategorie);
+        Rezept r1 = new Rezept(rezeptNamen, rezeptBeschreibung, rezeptZutaten, speisekarte1, kategorieObj);
 
         Mitarbeiter m1 = new Mitarbeiter(mitarbeiterNamen, mitarbeiterVornamen);
         r1.setSpeisekarte(speisekarte1);
